03aab63c1d6b621f9eb58531554ef2e0ac9b57d0
Okidoki0110 <37806242+Okidoki0110@users.noreply.github.com>
2019-06-17 18:52:43 +0300

    Add files via upload

--- orar.pl
+++ orar.pl
@@ -7,23 +7,18 @@
 % schedule(+Context, -Sol)
 % pentru contextul descris, întoarce o soluție care respectă
 % constrângerile fizice și de curiculă.
-schedule(Context, Sol) :- member(Act, Context), Act = activities([]), Sol = (Context, []).
+schedule(_, _) :- fail.
 
-
-schedule(_Context, _Sol) :- fail.
-
-
-% cost(+Sol, -Cost)
-% pentru soluția dată, întoarce costul implicat de constrângerile de
-% preferință care au fost încălcate.
-cost((_, []), 0).
-cost(_, _) :- fail.
+% schedule(+Context, -Sol, -Cost)
+% pentru contextul descris, întoarce o soluție care respectă
+% constrângerile fizice și de curiculă și calculează costul implicat de
+% constrângerile de preferință care au fost încălcate.
+schedule(_, _, _) :- fail.
 
 % schedule_best(+Context, -Sol, -Cost)
 % pentru contextul descris, întoarce soluția validă cu cel mai bun (cel
 % mai mic) cost (sau una dintre ele, dacă există mai multe cu același
 % cost)
-schedule_best(Context, (Context, []), 0) :- member(Act, Context), Act = activities([]).
 schedule_best(_, _, _) :- fail.
 
 
